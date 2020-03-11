f5fcc6dfaef48a7e747d7d90201154f7ab9d12e2
Hannah Stahl <hls62@cornell.edu>
2020-03-11 15:19:11 -0500

    Added contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import config from '../config';
 
-const Contact = () => (
-  <div>
-    <h1>Contact Page</h1>
-    <p>Insert contact form here.</p>
-  </div>
-);
+const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [buttonText, setButtonText] = useState('Send');
+
+  function updateName(e) {
+    setName(e.target.value);
+    setButtonText('Send');
+  }
+
+  function updateEmail(e) {
+    setEmail(e.target.value);
+    setButtonText('Send');
+  }
+
+  function updateMessage(e) {
+    setMessage(e.target.value);
+    setButtonText('Send');
+  }
+
+  function validateForm() {
+    return name.length > 0 && email.length > 0 && message.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setButtonText('Sending...');
+    fetch(config.emailURL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name,
+        email,
+        message,
+        sourceEmail: config.emailAddress,
+        siteDomain: window.location.origin,
+      }),
+    }).then((response) => response.json()).then((json) => {
+      if (json.MessageId) {
+        setButtonText('Sent!');
+        setName('');
+        setEmail('');
+        setMessage('');
+      } else {
+        setButtonText('Send');
+        window.alert('An error occurred with the contact form. Please send an email directly to steveparham@gmail.com.');
+      }
+    });
+  }
+
+  return (
+    <div className="contact">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="name">
+          <FormControl
+            type="text"
+            placeholder="Your Name"
+            value={name}
+            onChange={updateName}
+          />
+        </FormGroup>
+        <FormGroup controlId="email">
+          <FormControl
+            placeholder="Your Email Address"
+            type="email"
+            value={email}
+            onChange={updateEmail}
+          />
+        </FormGroup>
+        <FormGroup controlId="message">
+          <FormControl
+            rows={10}
+            as="textarea"
+            placeholder="Your Message"
+            value={message}
+            onChange={updateMessage}
+          />
+        </FormGroup>
+        <div>
+          <Button
+            block
+            type="submit"
+            size="lg"
+            variant="outline-light"
+            disabled={!validateForm()}
+          >
+            {buttonText}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
---
+++ src/config.js
@@ -0,0 +1,26 @@
+const dev = {
+  apiURL: 'https://dlnum6f7kj.execute-api.us-east-1.amazonaws.com/dev',
+  cloudfrontBaseURL: 'https://d1ljva6zkf6zjh.cloudfront.net',
+  emailURL: 'https://c0mrk8va37.execute-api.us-east-1.amazonaws.com/dev/email/send',
+  emailAddress: 'hannahstahl14@gmail.com',
+  userID: 'us-east-1:34ee9094-c95a-4f8f-b2c2-551ef33bd49f',
+};
+
+const prod = {
+  apiURL: 'https://lbe32id9hg.execute-api.us-east-1.amazonaws.com/prod',
+  cloudfrontBaseURL: 'https://d1esxin5o90ebg.cloudfront.net',
+  emailURL: 'https://c0mrk8va37.execute-api.us-east-1.amazonaws.com/dev/email/send', // TODO change to prod URL after promoting on Seed
+  emailAddress: 'hannahstahl14@gmail.com', // TODO change to steveparham@gmail.com later
+  userID: 'us-east-1:e51b2b71-8b21-46c0-a302-6f3b84783041', // TODO change to Steve's later
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  cloudfrontURL: `${config.cloudfrontBaseURL}/${config.userID}`,
+  ...config,
+};
