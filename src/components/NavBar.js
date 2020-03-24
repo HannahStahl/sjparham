252f81d7a2df91620b880347e9d9fd990735fc08
Hannah Stahl <hls62@cornell.edu>
2020-03-24 18:32:09 -0500

    Changed nav links to only be hoverable/clickable on the text

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,43 +17,48 @@ const NavBar = () => (
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto mr-auto" activeKey={`/${window.location.pathname.split('/')[1]}`}>
-          <Nav.Link
-            to={{ pathname: '/about', state: { prevPathname: window.location.pathname } }}
-            as={NavLink}
-            className="link-to-page"
-          >
-            <p>About</p>
-          </Nav.Link>
-          <Nav.Link
-            to={{ pathname: '/galleries', state: { prevPathname: window.location.pathname } }}
-            as={NavLink}
-            className="link-to-page"
-          >
-            <p>Galleries</p>
-          </Nav.Link>
-          <Nav.Link
-            to={{ pathname: '/', state: { prevPathname: window.location.pathname } }}
-            as={NavLink}
-            className="logo"
-          >
-            <p className="line-1">S J PARHAM</p>
-            <p className="line-2">PHOTOGRAPHY</p>
-          </Nav.Link>
-          <Nav.Link
-            to={{ pathname: '/contact', state: { prevPathname: window.location.pathname } }}
-            as={NavLink}
-            className="link-to-page"
-          >
-            <p>Contact</p>
-          </Nav.Link>
-          <Nav.Link
-            href="https://www.instagram.com/steve.parham.photography/"
-            className="link-to-page"
-            target="_blank"
-            rel="noopener nereferrer"
-          >
-            <p>Instagram</p>
-          </Nav.Link>
+          <div className="nav-link-container link-to-page">
+            <Nav.Link
+              to={{ pathname: '/about', state: { prevPathname: window.location.pathname } }}
+              as={NavLink}
+            >
+              <p>About</p>
+            </Nav.Link>
+          </div>
+          <div className="nav-link-container link-to-page">
+            <Nav.Link
+              to={{ pathname: '/galleries', state: { prevPathname: window.location.pathname } }}
+              as={NavLink}
+            >
+              <p>Galleries</p>
+            </Nav.Link>
+          </div>
+          <div className="nav-link-container logo">
+            <Nav.Link
+              to={{ pathname: '/', state: { prevPathname: window.location.pathname } }}
+              as={NavLink}
+            >
+              <p className="line-1">S J PARHAM</p>
+              <p className="line-2">PHOTOGRAPHY</p>
+            </Nav.Link>
+          </div>
+          <div className="nav-link-container link-to-page">
+            <Nav.Link
+              to={{ pathname: '/contact', state: { prevPathname: window.location.pathname } }}
+              as={NavLink}
+            >
+              <p>Contact</p>
+            </Nav.Link>
+          </div>
+          <div className="nav-link-container link-to-page">
+            <Nav.Link
+              href="https://www.instagram.com/steve.parham.photography/"
+              target="_blank"
+              rel="noopener nereferrer"
+            >
+              <p>Instagram</p>
+            </Nav.Link>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
