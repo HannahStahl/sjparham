644e962654890d6e64dfe8c93a3338336a6a969a
Hannah Stahl <hls62@cornell.edu>
2022-04-10 16:50:43 -0500

    Functional Sanitization script

--- populateSanityContent.js
+++ populateSanityContent.js
@@ -1,90 +1,109 @@
 import fetch from 'node-fetch';
 import sanityClient from '@sanity/client';
 
-const apiUrl = 'https://hntqppr7a3.execute-api.us-east-1.amazonaws.com/prod';
+const apiUrl = 'https://lbe...'; // Fetch this from Seed
+const userId = 'us-east-1:c48...'; // Fetch this from AWS
+const cloudfrontUrl = `https://d1esxin5o90ebg.cloudfront.net/${userId}`;
 const client = sanityClient({
-  projectId: 'kpu3h10o',
+  projectId: '4dw...', // Fetch this from Sanity
   dataset: 'production',
   apiVersion: '2021-10-03',
-  token: 'TODO', // starts with skk
+  token: 'skj...', // Fetch this from Notes
   useCdn: false,
 });
 
 const clearContent = async () => {
-  const sanityItems = await client.fetch('*[_type == "items"]');
-  const sanityCategories = await client.fetch('*[_type == "category"]');
+  console.log('Deleting existing content');
+  const sanityGalleries = await client.fetch('*[_type == "gallery"]');
   const sanityPhotos = await client.fetch('*[_type == "photo"]');
 
-  const sanityDocuments = [
-    ...sanityItems,
-    ...sanityCategories,
-    ...sanityPhotos,
-  ];
+  const sanityDocuments = [...sanityGalleries, ...sanityPhotos];
 
   for (const document of sanityDocuments) {
     await client.delete(document._id);
   }
 };
 
+const uploadImage = async (fileName) => {
+  const image = await fetch(`${cloudfrontUrl}/${fileName}`);
+  const imageBuffer = await image.arrayBuffer();
+  const imageData = Buffer.from(imageBuffer, 'binary');
+  return await client.assets.upload(
+    'image',
+    imageData,
+    { filename: fileName.substring(fileName.indexOf('-') + 1, fileName.lastIndexOf('.')) }
+  );
+};
+
 const populateContent = async () => {
-  const categories = await fetch(`${apiUrl}/categories`).then((res) => res.json());
-  const photos = await fetch(`${apiUrl}/photos`).then((res) => res.json());
+  const categories = await fetch(`${apiUrl}/publishedCategories/${userId}`).then((res) => res.json());
+  const items = await fetch(`${apiUrl}/publishedItems/${userId}`).then((res) => res.json());
+  const itemsToPhotos = await fetch(`${apiUrl}/itemsToPhotos/${userId}`).then((res) => res.json());
+  const photos = await fetch(`${apiUrl}/photos/${userId}`).then((res) => res.json());
 
-  const sortedCategories = categories.sort((a, b) => a.categoryRank - b.categoryRank);
+  const sortedCategories = categories.sort((a, b) => b.categoryRank - a.categoryRank);
 
-  const populatePhotoContent = async (photo) => {
-    const { photoId } = photo;
+  const populatePhotoContent = async (item) => {
+    const { itemDescription, itemId, itemName } = item;
+    console.log(`Processing item ${itemName}`);
 
-    await client.createOrReplace({
+    const joinRecord = itemsToPhotos.find((itemToPhoto) => itemToPhoto.itemId === itemId);
+    const photo = photos.find(({ photoId }) => joinRecord.photoId === photoId);
+
+    const image = await uploadImage(photo.photoName);
+
+    const photoContent = await client.createOrReplace({
       _type: 'photo',
-      _id: id, // TODO prepend with 'drafts.' if it's a draft photo
-      // TODO add other fields
+      _id: itemId,
+      _key: itemId,
+      title: itemName,
+      description: itemDescription,
+      image: {
+        _type: 'image',
+        asset: {
+          _type: 'reference',
+          _ref: image._id,
+        },
+      },
     });
-    
-    return {
-      _type: 'reference',
-      _ref: id,
-      _key: id,
-    };
+
+    return photoContent;
   };
 
   const populateCategoryContent = async (category) => {
-    const { categoryId } = category;
+    const { categoryId, categoryName, categoryPhoto } = category;
+    console.log(`Processing category ${categoryName}`);
 
-    const photosInThisCategory = photos.filter((photo) => photo.categoryId === categoryId);
+    const previewImage = await uploadImage(categoryPhoto);
+
+    const itemsInThisCategory = items.filter((photo) => photo.categoryId === categoryId);
+    const sortedItems = itemsInThisCategory.sort((a, b) => a.itemRank - b.itemRank);
     const photosContent = [];
-    for (const photo of photosInThisCategory) {
-      const photoContent = await populatePhotoContent(photo);
+    for (const item of sortedItems) {
+      const photoContent = await populatePhotoContent(item);
       photosContent.push(photoContent);
     }
 
     await client.createOrReplace({
-      _type: 'category',
-      _id: id, // TODO prepend with 'drafts.' if it's a draft category
-      // TODO add other fields
+      _type: 'gallery',
+      _id: categoryId,
+      name: categoryName,
+      previewPhoto: {
+        _type: 'image',
+        asset: {
+          _type: 'reference',
+          _ref: previewImage._id,
+        },
+      },
       photos: photosContent,
     });
-
-    return {
-      _type: 'reference',
-      _ref: id,
-      _key: id,
-    };
   };
 
   await clearContent();
 
-  const categoriesContent = [];
   for (const category of sortedCategories) {
-    const categoryContent = await populateCategoryContent(category);
-    categoriesContent.push(categoryContent);
+    await populateCategoryContent(category);
   }
-
-  await client.createOrReplace({
-    _type: 'categories',
-    _id: 'categories',
-    categories: categoriesContent,
-  });
 };
 
 populateContent().then(() => console.log('Done populating content!'));
